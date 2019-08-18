173c0e261f9f1ae5a60e2ca3b0ee5e8aa068e9a9
Jennifer Rink <jen@Jennifers-MBP.fritz.box>
2019-08-18 18:10:43 +0200

    reparatur prozess

--- script.js
+++ script.js
@@ -39,56 +39,56 @@ $(document).ready(function() {
         $("#tractor2, #tractor3, #tractor4,#tractor5, #tractor6, #tractor7").css("visibility", "hidden");
         $(".philosophie").css("color", "#57CF95");
         $(".abo, .service, .prozess, .community, .partner, .ui").css("color", "#203959");
-        $(".rechteSeite h3").text("Philosophie");
-        $(".rechteSeite p").text("Die landchef Philosophie beruht zum einen darauf, die Region zu stärken und Persönlichkeit und Wertschätzung unter den Menschen zu fördern. Daher ist die landchef Partnerschaft die Grundlage für das landchef Konzept. Zum anderen möchte landchef Landwirte in ihrer Arbeit unterstützten und über die Direktvermarktung Alternativen für ein sicheres Einkommen erleichtern. Dabei legt das landchef Team besonderen Wert, den Menschen stupide Arbeit abzunehmen, um Zeit für persönliche Arbeit und Beziehungen zu schenken. <br> Außerdem möchte das landchef Team als Vorbild fungieren und partnerschaftlich mit seinen eigenen Kunden umgehen. So erhält Ehrlichkeit eine besondere Bedeutung in der Gestaltung des landchef Konzepts.");
+        $("#textPhilosophie").css("display","block");
+        $("#textPartner, #textCommunity, #textProzess, #textNutzererlebnis, #textAbo, #textService").css("display","none");
     });
     $(".partner").click(function(){
         $("#tractor1, #tractor3, #tractor4,#tractor5, #tractor6, #tractor7").css("visibility", "hidden");
         $("#tractor2").css("visibility", "visible");
         $(".abo, .service, .prozess, .community, .philosophie, .ui").css("color", "#203959");
         $(".partner").css("color", "#57CF95");
-        $(".rechteSeite h3").text("Partnerschaft");
-        $(".rechteSeite p").text("Das landchef Konzept basiert auf Partnerschaften, um die Persönlichkeit in der Region zu fördern und einen großen Wunsch der Nutzer von landchef zu erfüllen, Nähe zum Kunden, der zuverlässig und vertrauensvoll ist. Daher bietet landchef eine Plattform auf der Partnerschaften zwischen Gastronomen und Landwirten entstehen und gefestigt werden können. Um das Ziel zu erreichen, besteht das landchef Konzept darauf, dass Erzeuger und Abnehmer zu beginn einer Partnerschaft persönlich miteinander kommunizieren. Erst nach der initialen Kontaktaufnahme wird dem Gastronom die Möglichkeit gegeben einen Landwirt als Partner anzufragen. Viele der heutigen Prozesse des Warenbezugs sind stark abstrahiert. So werden jegliche Emotionen und Bindungen untereinander weg-digitalisiert.");
+        $("#textPartner").css("display","block");
+        $("#textPhilosophie, #textCommunity, #textProzess, #textNutzererlebnis, #textAbo, #textService").css("display","none");
     });
     $(".community").click(function(){
         $("#tractor1, #tractor2, #tractor3, #tractor4,#tractor5, #tractor6").css("visibility", "hidden");
         $("#tractor7").css("visibility", "visible");
         $(".abo, .service, .prozess, .partner, .philosophie, .ui").css("color", "#203959");
         $(".community").css("color", "#57CF95");
-        $(".rechteSeite h3").text("Community");
-        $(".rechteSeite p").text("Als Soziales Netzwerk bietet landchef mehrere Möglichkeiten der Vernetzung. Nicht nur Gastronomen und Landwirte können sich verpartnern, sondern auch Landwirte und Gastronomen untereinander. Dies ermöglicht es den beiden Gruppen in Kontakt zu bleiben und sich auszutauschen. Besonders für Landwirte, die derzeit bereits oft untereinander vernetzt und organisiert sind, ist dies eine interessante Funktion.");
+        $("#textCommunity").css("display","block");
+        $("#textPhilosophie, #textPartner, #textProzess, #textNutzererlebnis, #textAbo, #textService").css("display","none");
     });
     $(".prozess").click(function(){
         $("#tractor1, #tractor2, #tractor4,#tractor5, #tractor6, #tractor7").css("visibility", "hidden");
         $("#tractor3").css("visibility", "visible");
         $(".abo, .service, .community, .partner, .philosophie, .ui").css("color", "#203959");
         $(".prozess").css("color", "#57CF95");
-        $(".rechteSeite h3").text("Prozess");
-        $(".rechteSeite p").text("Die landchef App unterstützt etablierten Direktvermarkter im Kampf mit Papierkram und Bestellorganisation. Durch eine kommunikationsorientierte Plattform werden repetitive Organisationsaufgaben vereinfacht, um mehr Platz für direkte Kommunikation und damit Nähe zum Kunden herzustellen. Darunter gehören Kontaktaufnahme zu potentiellen Partnern, individuelle Angebotserstellung sowie die Abwicklung der Bestellprozesse.");
+        $("#textProzess").css("display","block");
+        $("#textPhilosophie, #textPartner, #textCommunity, #textNutzererlebnis, #textAbo, #textService").css("display","none");
     });
     $(".ui").click(function(){
         $("#tractor1, #tractor2, #tractor3, #tractor4,#tractor5, #tractor7").css("visibility", "hidden");
         $("#tractor6").css("visibility", "visible");
         $(".ui").css("color", "#57CF95");
         $(".abo, .service, .prozess, .community, .partner, .philosophie").css("color", "#203959");
-        $(".rechteSeite h3").text("Nutzererlebnis");
-        $(".rechteSeite p").text("Die Kernkompetenz von landchef stützt sich auf das Nutzererlebnis, denn die App baut hauptsächlich auf einen abgerundeten und zuverlässigen Ablauf auf. Das bedeutet, Funktionalität und Benutzerfreundlichkeit haben die höchste Priorität für das Tool sowie für denjenigen, der sie verwendet. Damit du deine Aufgaben ohne Hindernisse, Probleme oder ähnliches, das dich behindern könnte, erledigen kannst.");
+        $("#textNutzererlebnis").css("display","block");
+        $("#textPhilosophie, #textPartner, #textCommunity, #textProzess, #textAbo, #textService").css("display","none");
     });
     $(".abo").click(function(){
         $("#tractor1, #tractor2, #tractor3, #tractor5, #tractor6, #tractor7").css("visibility", "hidden");
         $("#tractor4").css("visibility", "visible");
         $(".service, .prozess, .community, .partner, .philosophie, .ui").css("color", "#203959");
         $(".abo").css("color", "#57CF95");
-        $(".rechteSeite h3").text("Abo-Modell");
-        $(".rechteSeite p").text("Die landchef App ist in einem Jahres-Abo erhältlich und bietet drei Pakete mit unterschiedlichen Leistungen an – das Starter, Basic und Pro Paket. Alle Pakete enthalten die Nutzung der landchef Community sowie das Bestelltool, welches über die Pakete unterschiedlich viele Partnerschaften und Bestellungen gewährt. Vor dem ersten Kauf der App besteht die Möglichkeit eines Probemonats, um die Funktionen der landchef App für die eigenen Zwecke zu prüfen. Ist der Monat abgelaufen, können die letzten Bestellungen noch abgeschlossen, aber keine neuen erstellt werden, oder die landchef wird abonniert.");
+        $("#textAbo").css("display","block");
+        $("#textPhilosophie, #textPartner, #textCommunity, #textProzess, #textNutzererlebnis, #textService").css("display","none");
     });
     $(".service").click(function(){
         $("#tractor1, #tractor2, #tractor3, #tractor4, #tractor6, #tractor7").css("visibility", "hidden");
         $("#tractor5").css("visibility", "visible");
         $(".abo, .prozess, .community, .partner, .philosophie, .ui").css("color", "#203959");
         $(".service").css("color", "#57CF95");
-        $(".rechteSeite h3").text("Service & Funktionalität");
-        $(".rechteSeite p").text("Das Leben der Nutzer von landchef zu vereinfachen und Zeit zu sparen, gehört zur landchef Philosophie, daher ist dem landchef Team besonderen wichtig, dass die Funktionalität der App zuverlässig abläuft. Darüberhinaus wird sehr stark auf engen Kontakt zu den Nutzer geachtet, um die landchef App stetig für diese zu verbessern und an ihre Lebenssituationen entsprechend anzupassen. Außerdem steht das landchef Team bei Fragen oder Problemen gerne stets zur Verfügung.");
+        $("#textService").css("display","block");
+        $("#textPhilosophie, #textPartner, #textCommunity, #textProzess, #textNutzererlebnis, #textAbo").css("display","none");
     });
 });
 
@@ -160,7 +160,7 @@ $(document).ready(function() {
 
 
     // Smiley
-    $("#zeitstrahl").hover(function(){
+    $("#zeitstrahl, #smiley").hover(function(){
         $("#smiley").css("visibility", "visible");
     }, function() {
         $("#smiley").css("visibility", "hidden");
