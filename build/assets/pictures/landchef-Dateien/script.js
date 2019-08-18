88f6377bc904254278df77df954dceeb5246e8da
Patrik Keufen <pk@bevelop.de>
2019-08-18 23:07:20 +0200

    fixed untracked files

--- build/assets/pictures/landchef-Dateien/script.js
+++
@@ -1,9 +0,0 @@
-//get access to the button and set up a click event handler
-var button = document.getElementById("submit");
-button.onclick = changeGreeting;
-
-function changeGreeting(){
-    var greeting = document.getElementsByTagName("h1")[0];
-    var input = document.getElementById("customer-amount").value;
-    greeting.innerHTML = "Hello " + input;
-}
