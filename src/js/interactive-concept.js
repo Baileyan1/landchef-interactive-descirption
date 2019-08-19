2ca6b82500c91c7ac6b2a3c4fa43be8e3fd7f0c2
Patrik Keufen <pk@bevelop.de>
2019-08-19 11:34:15 +0200

    added svg import

--- src/js/interactive-concept.js
+++ src/js/interactive-concept.js
@@ -1,23 +1,19 @@
-function conceptReel() {
-  $(document).ready(function() {
-    const firstActive = $("#concept-reel").attr("data-first-id");
+$(document).ready(function() {
+  const firstActive = $("#concept-reel").attr("data-first-id");
 
-    $(`#concept-reel .text .item[data-id=${firstActive}]`).addClass("active");
-    $(`#concept-reel .illustration .item[data-id=${firstActive}]`).addClass(
-      "active"
-    );
+  $(`#concept-reel .text .item[data-id=${firstActive}]`).addClass("active");
+  $(`#concept-reel .illustration .item[data-id=${firstActive}]`).addClass(
+    "active"
+  );
 
-    $("#concept-reel .illustration .item").click(function() {
-      // deactivate all
-      $("#concept-reel .illustration .item").removeClass("active");
-      $("#concept-reel .text .item").removeClass("active");
+  $("#concept-reel .illustration .item").click(function() {
+    // deactivate all
+    $("#concept-reel .illustration .item").removeClass("active");
+    $("#concept-reel .text .item").removeClass("active");
 
-      // activate clicked
-      $(this).addClass("active");
-      const newActive = $(this).attr("data-id");
-      $(`#concept-reel .text .item[data-id=${newActive}]`).addClass("active");
-    });
+    // activate clicked
+    $(this).addClass("active");
+    const newActive = $(this).attr("data-id");
+    $(`#concept-reel .text .item[data-id=${newActive}]`).addClass("active");
   });
-}
-
-conceptReel();
+});
