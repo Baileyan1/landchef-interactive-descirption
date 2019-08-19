b841cfe77f6f0e5bf16532f03fdb6cd99b28f715
Patrik Keufen <pk@bevelop.de>
2019-08-19 02:14:41 +0200

    reworked concept and added layout classes

--- src/js/interactive-concept.js
+++ src/js/interactive-concept.js
@@ -34,117 +34,22 @@ jQuery('img[src$=".svg"]').each(function() {
 // das konzept - interaktiv
 
 $(document).ready(function() {
-  $(".philosophie h4, .philosophie img").click(function() {
-    $("#tractor1").css("visibility", "visible");
-    $("#tractor2, #tractor3, #tractor4,#tractor5, #tractor6, #tractor7").css(
-      "visibility",
-      "hidden"
-    );
-    $(".philosophie").css("color", "#57CF95");
-    $(".abo, .service, .prozess, .community, .partner, .ui").css(
-      "color",
-      "#203959"
-    );
-    $("#textPhilosophie").css("display", "block");
-    $(
-      "#textPartner, #textCommunity, #textProzess, #textNutzererlebnis, #textAbo, #textService"
-    ).css("display", "none");
-  });
-  $(".partner h4, .partner img").click(function() {
-    $("#tractor1, #tractor3, #tractor4,#tractor5, #tractor6, #tractor7").css(
-      "visibility",
-      "hidden"
-    );
-    $("#tractor2").css("visibility", "visible");
-    $(".abo, .service, .prozess, .community, .philosophie, .ui").css(
-      "color",
-      "#203959"
-    );
-    $(".partner").css("color", "#57CF95");
-    $("#textPartner").css("display", "block");
-    $(
-      "#textPhilosophie, #textCommunity, #textProzess, #textNutzererlebnis, #textAbo, #textService"
-    ).css("display", "none");
-  });
-  $(".community h4, .community img").click(function() {
-    $("#tractor1, #tractor2, #tractor3, #tractor4,#tractor5, #tractor6").css(
-      "visibility",
-      "hidden"
-    );
-    $("#tractor7").css("visibility", "visible");
-    $(".abo, .service, .prozess, .partner, .philosophie, .ui").css(
-      "color",
-      "#203959"
-    );
-    $(".community").css("color", "#57CF95");
-    $("#textCommunity").css("display", "block");
-    $(
-      "#textPhilosophie, #textPartner, #textProzess, #textNutzererlebnis, #textAbo, #textService"
-    ).css("display", "none");
-  });
-  $(".prozess h4, .prozess img").click(function() {
-    $("#tractor1, #tractor2, #tractor4,#tractor5, #tractor6, #tractor7").css(
-      "visibility",
-      "hidden"
-    );
-    $("#tractor3").css("visibility", "visible");
-    $(".abo, .service, .community, .partner, .philosophie, .ui").css(
-      "color",
-      "#203959"
-    );
-    $(".prozess").css("color", "#57CF95");
-    $("#textProzess").css("display", "block");
-    $(
-      "#textPhilosophie, #textPartner, #textCommunity, #textNutzererlebnis, #textAbo, #textService"
-    ).css("display", "none");
-  });
-  $(".ui h4, .ui img").click(function() {
-    $("#tractor1, #tractor2, #tractor3, #tractor4,#tractor5, #tractor7").css(
-      "visibility",
-      "hidden"
-    );
-    $("#tractor6").css("visibility", "visible");
-    $(".ui").css("color", "#57CF95");
-    $(".abo, .service, .prozess, .community, .partner, .philosophie").css(
-      "color",
-      "#203959"
-    );
-    $("#textNutzererlebnis").css("display", "block");
-    $(
-      "#textPhilosophie, #textPartner, #textCommunity, #textProzess, #textAbo, #textService"
-    ).css("display", "none");
-  });
-  $(".abo h4, .abo img").click(function() {
-    $("#tractor1, #tractor2, #tractor3, #tractor5, #tractor6, #tractor7").css(
-      "visibility",
-      "hidden"
-    );
-    $("#tractor4").css("visibility", "visible");
-    $(".abo").css("color", "#57CF95");
-    $(".service, .prozess, .community, .partner, .philosophie, .ui").css(
-      "color",
-      "#203959"
-    );
-    $("#textAbo").css("display", "block");
-    $(
-      "#textPhilosophie, #textPartner, #textCommunity, #textProzess, #textNutzererlebnis, #textService"
-    ).css("display", "none");
-  });
-  $(".service h4, .service img").click(function() {
-    $("#tractor1, #tractor2, #tractor3, #tractor4, #tractor6, #tractor7").css(
-      "visibility",
-      "hidden"
-    );
-    $("#tractor5").css("visibility", "visible");
-    $(".abo, .prozess, .community, .partner, .philosophie, .ui").css(
-      "color",
-      "#203959"
-    );
-    $(".service").css("color", "#57CF95");
-    $("#textService").css("display", "block");
-    $(
-      "#textPhilosophie, #textPartner, #textCommunity, #textProzess, #textNutzererlebnis, #textAbo"
-    ).css("display", "none");
+  const firstActive = $("#concept-reel").attr("data-first-id");
+
+  $(`#concept-reel .text .item[data-id=${firstActive}]`).addClass("active");
+  $(`#concept-reel .illustration .item[data-id=${firstActive}]`).addClass(
+    "active"
+  );
+
+  $("#concept-reel .illustration .item").click(function() {
+    // deactivate all
+    $("#concept-reel .illustration .item").removeClass("active");
+    $("#concept-reel .text .item").removeClass("active");
+
+    // activate clicked
+    $(this).addClass("active");
+    const newActive = $(this).attr("data-id");
+    $(`#concept-reel .text .item[data-id=${newActive}]`).addClass("active");
   });
 });
 
