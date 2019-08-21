754805eaf087abff38f957d2e293147bb4b5edf1
Patrik Keufen <pk@bevelop.de>
2019-08-21 13:09:42 +0200

    added hover effect to list

--- src/js/07_draggable.js
+++ src/js/07_draggable.js
@@ -1,13 +1,8 @@
-function sortable(){
-    $(document).ready(() => {
-      
-    var el = document.getElementById('order-list');
-    var sortable = Sortable.create(el);
-
-        new Sortibal(order-list, {
-            animation: 150,
-            ghostClass: 'blue-background-class'
-        })
-  
+function sortable() {
+  $(document).ready(() => {
+    var el = document.getElementById("order-list");
+    var sortable = Sortable.create(el, {
+      animation: 150
     });
+  });
 }
