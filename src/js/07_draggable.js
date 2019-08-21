2612518111fe455933755a581e5856e3d5661ecc
Patrik Keufen <pk@bevelop.de>
2019-08-21 16:47:36 +0200

    last item now disappears

--- src/js/07_draggable.js
+++ src/js/07_draggable.js
@@ -3,8 +3,28 @@ function sortable() {
     var el = document.getElementById("order-list");
     var sortable = Sortable.create(el, {
       animation: 150,
-      filter: "h4",
-      ghostClass: "placeholder"
+      filter: "h4, .inactive",
+      ghostClass: "placeholder",
+      onEnd: function(/**Event*/ evt) {
+        const listLength = $("#order-list .item").length;
+
+        if (
+          $("#order-list .last")
+            .nextAll()
+            .filter($(evt.item)).length !== 0
+        ) {
+          $(evt.item)
+            .addClass("inactive")
+            .slideUp(300, function() {
+              $(evt.item).prependTo($("#order-list"));
+              setTimeout(() => {
+                $(evt.item)
+                  .slideDown()
+                  .removeClass("inactive");
+              }, 10000);
+            });
+        }
+      }
     });
   });
 }
