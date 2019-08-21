3fc9a621a61735632afd2ce745bdab83f98e1095
Patrik Keufen <pk@bevelop.de>
2019-08-21 15:12:45 +0200

    added animations and tick

--- src/js/07_draggable.js
+++ src/js/07_draggable.js
@@ -2,7 +2,9 @@ function sortable() {
   $(document).ready(() => {
     var el = document.getElementById("order-list");
     var sortable = Sortable.create(el, {
-      animation: 150
+      animation: 150,
+      filter: "h4",
+      ghostClass: "placeholder"
     });
   });
 }
